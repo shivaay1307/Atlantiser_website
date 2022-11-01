5aba7aaff7bf0e72537bdc6426390533b2626806
shivaay1307 <sgrgsggg@gmail.com>
2022-11-01 17:58:27 +0530

    [insatllation]

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/global.css"
+
+export default function Home() {
+  return <div className="text-3xl font-bold text-white bg-gray-700">Hello world!</div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/index.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
